7460f1e3e07e6056e39f3bec63ebfbf711c3ba8d
Ruslan <yaarusik@gmail.com>
2021-11-03 16:42:04 +0500

    fix: task 2 - delete inner text.txt

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,8 +2,18 @@ const fs = require("fs");
 const path = require("path");
 
 const { stdin, stdout } = process;
+const fileText = path.join(__dirname, "text.txt");
 
 stdout.write("Пожалуйста введите текст:\n");
+
+fs.access(fileText, (err) => {
+  if (err) {
+  } else {
+    fs.truncate(fileText, (err) => {
+      if (err) throw err;
+    });
+  }
+});
 
 stdin.on("data", (data) => {
   // stdout.write(data);
@@ -14,7 +24,7 @@ stdin.on("data", (data) => {
     process.exit();
   }
 
-  fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+  fs.appendFile(fileText, data, (err) => {
     if (err) {
       throw err;
     }
